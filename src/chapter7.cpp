70cbbfdd88169e71c7ed986c13905a33b7393e77
fengjiran <fengjiran@foxmail.com>
2018-11-11 19:17:15 +0800

    fill an array

--- src/chapter7.cpp
+++ src/chapter7.cpp
@@ -28,3 +28,23 @@ void ex1() {
         }
     }
 }
+
+int FillArray(double arr[], int limit) {
+    double temp;
+    int i;
+    for (i = 0; i < limit; i++) {
+        cout << "Enter value #" << (i + 1) << ": ";
+        cin >> temp;
+        if (!cin) {  // bad input
+            cin.clear();
+            while (cin.get() != '\n')
+                continue;
+            cout << "Bad input; input process terminated.\n";
+            break;
+        } else if (temp < 0)
+            break;
+        arr[i] = temp;
+    }
+
+    return i;
+}
