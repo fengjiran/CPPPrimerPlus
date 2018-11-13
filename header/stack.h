a714a17dbabb385f339501f5692e9db4e9bc6626
richard <fengjiran@foxmail.com>
2018-11-14 02:19:53 +0800

    stack class

--- header/stack.h
+++ header/stack.h
@@ -5,4 +5,18 @@
 #ifndef CPPPRIMERPLUS_STACK_H
 #define CPPPRIMERPLUS_STACK_H
 
+typedef unsigned long Item;
+
+class Stack {
+private:
+    static const int max = 10;
+    Item items[max];  // hold stack items
+    int top;  // index for top stack item
+public:
+    Stack();
+    bool IsEmpty() const;
+    bool IsFull() const;
+    bool Push(const Item & item);  // add item to stack
+    bool Pop(Item &item);  // pop top into item
+};
 #endif //CPPPRIMERPLUS_STACK_H
