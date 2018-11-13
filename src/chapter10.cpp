fc1ceff665446d28e80b8db6b099a515c86d4ed9
richard <fengjiran@foxmail.com>
2018-11-14 01:06:32 +0800

    use stock2

--- src/chapter10.cpp
+++ src/chapter10.cpp
@@ -2,6 +2,7 @@
 // Created by Richard on 2018/11/9.
 //
 #include "../header/stock00.h"
+
 using namespace std;
 
 void UseStock0() {
@@ -26,5 +27,22 @@ void UseStock0() {
 }
 
 void UseStock2() {
-    //
+    const int STKS = 4;
+    Stock stocks[STKS] = {
+            Stock("NanoSmart", 12, 20.0),
+            Stock("Boffo Objects", 200, 2.0),
+            Stock("Monolithic Obelisks", 130, 3.25),
+            Stock("Fleep Enterprises", 60, 6.5)
+    };
+    cout << "Stock holdings:\n";
+    int st;
+    for (st = 0; st < STKS; st++)
+        stocks[st].Show();
+
+    const Stock *top = &stocks[0];
+    for (st = 1; st < STKS; st++)
+        top = &stocks[st].GetTopVal(*top);
+
+    cout << "\nMost valuable holding:\n";
+    top->Show();
 }
