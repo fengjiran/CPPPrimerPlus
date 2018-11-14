52e941fabb980d73b373334816cf9db9f5f37578
fengjiran <fengjiran@foxmail.com>
2018-11-15 00:30:06 +0800

    add vector methods

--- src/vector.cpp
+++ src/vector.cpp
@@ -73,5 +73,53 @@ namespace VECTOR {
 
     }
 
+    void Vector::Reset(double n1, double n2, Vector::Mode form) {
+        this->mode = form;
+        if (this->mode == RECT) {
+            this->x = n1;
+            this->y = n2;
+            this->SetAng();
+            this->SetMag();
+        } else if (this->mode == POL) {
+            this->mag = n1;
+            this->ang = n2 / Rad_to_deg;
+            this->SetX();
+            this->SetY();
+        } else {
+            cout << "Incorrect 3rd argument to Vector() --";
+            cout << "vector set to 0\n";
+            this->x = 0.0;
+            this->y = 0.0;
+            this->mag = 0.0;
+            this->ang = 0.0;
+            this->mode = RECT;
+        }
+
+    }
+
+    Vector::~Vector() {
+
+    }
+
+    void Vector::SetPolarMode() {
+        this->mode = POL;
+    }
+
+    void Vector::SetRectMode() {
+        this->mode = RECT;
+    }
+
+    Vector Vector::operator+(const Vector &b) const {
+        return Vector(this->x + b.x, this->y + b.y);
+    }
+
+    Vector Vector::operator-(const Vector &b) const {
+        return Vector(this->x - b.x, this->y - b.y);
+    }
+
+    Vector Vector::operator-() const {
+        return Vector(-this->x, -this->y);
+    }
+
 
 }
