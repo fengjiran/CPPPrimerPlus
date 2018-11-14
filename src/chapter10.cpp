6eae747b10dba211919340ef6fa53b8e6096211b
fengjiran <fengjiran@foxmail.com>
2018-11-14 09:51:32 +0800

    testing the stack class

--- src/chapter10.cpp
+++ src/chapter10.cpp
@@ -52,5 +52,26 @@ void UseStock2() {
 void UseStack() {
     Stack st;  // create an empty stack
     char ch;
-
+    Item po;
+    cout << "Please enter A to add a purchase order.\n"
+         << "P to process a PO, or Q to quit.\n";
+    while (cin >> ch && toupper(ch) != 'Q') {
+        while (cin.get() != '\n')
+            continue;
+        if (!isalpha(ch)) {
+            cout << '\a';
+            continue;
+        }
+        switch (ch) {
+            case 'A':
+            case 'a':
+                cout << "Enter a PO number to add: ";
+                cin >> po;
+                if (st.IsFull())
+                    cout << "Stack already full\n";
+                else
+                    st.Push(po);
+                break;
+        }
+    }
 }
