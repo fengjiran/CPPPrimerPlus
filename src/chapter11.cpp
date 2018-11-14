132e7f0811d829d60527518e521aeb14b4212671
fengjiran <fengjiran@foxmail.com>
2018-11-14 19:06:20 +0800

    method for operator +

--- src/chapter11.cpp
+++ src/chapter11.cpp
@@ -28,3 +28,37 @@ void UseTime0() {
     total.Show();
     std::cout << std::endl;
 }
+
+void UseTime1() {
+    Time planning;
+    Time coding(2, 40);
+    Time fixing(5, 55);
+    Time total;
+
+    std::cout << "planning time = ";
+    planning.Show();
+    std::cout << std::endl;
+
+    std::cout << "coding time = ";
+    coding.Show();
+    std::cout << std::endl;
+
+    std::cout << "fixing time = ";
+    fixing.Show();
+    std::cout << std::endl;
+
+    total = coding + fixing;
+    std::cout << "coding + fixing = ";
+    total.Show();
+    std::cout << std::endl;
+
+    Time morefixing(3, 28);
+    std::cout << "more fixing time = ";
+    morefixing.Show();
+    std::cout << std::endl;
+
+    total = morefixing.operator+(total);
+    std::cout << "morefixing.operator+(total) = ";
+    total.Show();
+    std::cout << std::endl;
+}
