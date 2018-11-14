ce5838a8776a5c7d1f19fc2680b813c35a212013
fengjiran <fengjiran@foxmail.com>
2018-11-14 16:35:45 +0800

    add new methods

--- header/mytime0.h
+++ header/mytime0.h
@@ -5,4 +5,24 @@
 #ifndef CPPPRIMERPLUS_MYTIME0_H
 #define CPPPRIMERPLUS_MYTIME0_H
 
+class Time {
+private:
+    int hours;
+    int minutes;
+public:
+    Time();
+
+    Time(int h, int m = 0);
+
+    void AddMin(int m);
+
+    void AddHr(int h);
+
+    void Reset(int h = 0, int m = 0);
+
+    Time Sum(const Time &t) const;
+
+    void Show() const;
+};
+
 #endif //CPPPRIMERPLUS_MYTIME0_H
