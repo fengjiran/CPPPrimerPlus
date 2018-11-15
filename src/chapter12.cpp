dfa9aea999e520c64b28659216665ec8afc2d71f
fengjiran <fengjiran@foxmail.com>
2018-11-15 19:02:55 +0800

    add chapter 12 test methods

--- src/chapter12.cpp
+++ src/chapter12.cpp
@@ -6,6 +6,7 @@
 #include "../header/strngbad.h"
 
 using std::cout;
+using std::endl;
 
 void Callme1(StringBad &rsb) {
     cout << "String passed by reference:\n";
@@ -15,4 +16,34 @@ void Callme1(StringBad &rsb) {
 void Callme2(StringBad sb) {
     cout << "String passed by value:\n";
     cout << "   \"" << sb << "\"\n";
+}
+
+void Vegnews() {
+    {
+        cout << "Starting an inner block.\n";
+        StringBad headline1("Celery Stalks at Midnight");
+        StringBad headline2("Lettuce Prey");
+        StringBad sports("Spinach Leave Bowl for Dollars");
+
+        cout << "headline1: " << headline1 << endl;
+        cout << "headline2: " << headline2 << endl;
+        cout << "sports: " << sports << endl;
+
+        Callme1(headline1);
+        cout << "headline1: " << headline1 << endl;
+        Callme2(headline2);
+        cout << "headline2: " << headline2 << endl;
+
+        cout << "Initialize one object to another:\n";
+        StringBad sailor = sports;
+        cout << "sailor: " << sailor << endl;
+        cout << "Assign one object to another:\n";
+        StringBad knot;
+        knot = headline1;
+        cout << "knot: " << knot << endl;
+        cout << "Exiting the block.\n";
+
+    }
+
+    cout << "End of Vegnews()\n";
 }
