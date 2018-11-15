b0308e8d5e8fcc69270fd2ecaa436311248f6f90
fengjiran <fengjiran@foxmail.com>
2018-11-15 17:52:32 +0800

    declaration

--- header/strngbad.h
+++ header/strngbad.h
@@ -5,4 +5,21 @@
 #ifndef CPPPRIMERPLUS_STRNGBAD_H
 #define CPPPRIMERPLUS_STRNGBAD_H
 
+#include <iostream>
+
+class StringBad {
+private:
+    char *srt;
+    int len;
+    static int num_strings;
+public:
+    StringBad();
+
+    StringBad(const char *s);
+
+    ~StringBad();
+
+    friend std::ostream &operator<<(std::ostream &os, const StringBad &st);
+};
+
 #endif //CPPPRIMERPLUS_STRNGBAD_H
