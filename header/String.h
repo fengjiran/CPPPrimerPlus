2af9f180a966f0fbb61f0baa507e2565aa971093
fengjiran <fengjiran@foxmail.com>
2018-11-15 22:16:15 +0800

    declaration for String class

--- header/String.h
+++ header/String.h
@@ -5,4 +5,21 @@
 #ifndef CPPPRIMERPLUS_STRING_H
 #define CPPPRIMERPLUS_STRING_H
 
+#include <iostream>
+
+class String {
+private:
+    char *str;
+    int len;
+    static int num_strings;
+public:
+    String();
+
+    explicit String(const char *s);
+
+    ~String();
+
+    friend std::ostream &operator<<(std::ostream &os, const String &st);
+};
+
 #endif //CPPPRIMERPLUS_STRING_H
