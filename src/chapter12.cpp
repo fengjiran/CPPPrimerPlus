f53039c134f421155bffa1bce20a1948a50a9d21
fengjiran <fengjiran@foxmail.com>
2018-11-15 18:32:52 +0800

    add chapter 12 test methods

--- src/chapter12.cpp
+++ src/chapter12.cpp
@@ -2,3 +2,17 @@
 // Created by Richard on 2018/11/15.
 //
 
+#include <iostream>
+#include "../header/strngbad.h"
+
+using std::cout;
+
+void Callme1(StringBad &rsb) {
+    cout << "String passed by reference:\n";
+    cout << "   \"" << rsb << "\"\n";
+}
+
+void Callme2(StringBad sb) {
+    cout << "String passed by value:\n";
+    cout << "   \"" << sb << "\"\n";
+}
