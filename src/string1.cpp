c64df2640f8b9204c7972209ebc2392d308a4696
fengjiran <fengjiran@foxmail.com>
2018-11-17 16:57:36 +0800

    implement constructor

--- src/string1.cpp
+++ src/string1.cpp
@@ -2,4 +2,22 @@
 // Created by richard on 2018/11/17.
 //
 #include <cstring>
-#include "../header/"
+#include "../header/string1.h"
+
+using std::cin;
+using std::cout;
+
+int String::num_strings = 0;
+
+int String::HowMany() {
+    return num_strings;
+}
+
+String::String(const char *s) {
+    this->len = std::strlen(s);
+    this->str = new char[this->len + 1];
+    std::strcpy(str, s);
+    num_strings++;
+
+}
+
