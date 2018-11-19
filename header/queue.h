eba9e79e6e9c161a6aa6a047b33a3f61bfed4348
fengjiran <fengjiran@foxmail.com>
2018-11-19 14:42:55 +0800

    add Customer class methods

--- header/queue.h
+++ header/queue.h
@@ -5,4 +5,18 @@
 #ifndef CPPPRIMERPLUS_QUEUE_H
 #define CPPPRIMERPLUS_QUEUE_H
 
+class Customer {
+private:
+    long arrive;
+    int processtime;
+public:
+    Customer() { arrive = processtime = 0; }
+
+    void set(long when);
+
+    long when() const { return arrive; }
+
+    int ptime() const { return processtime; }
+};
+
 #endif //CPPPRIMERPLUS_QUEUE_H
