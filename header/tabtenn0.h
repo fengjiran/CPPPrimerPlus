1d852826d57a43f4f3be1976686dbd1b4d12be12
fengjiran <fengjiran@foxmail.com>
2018-11-19 19:26:42 +0800

    declaration

--- header/tabtenn0.h
+++ header/tabtenn0.h
@@ -5,4 +5,25 @@
 #ifndef CPPPRIMERPLUS_TABTENN0_H
 #define CPPPRIMERPLUS_TABTENN0_H
 
+#include <string>
+
+using std::string;
+
+class TableTennisPlayer {
+private:
+    string firstname;
+    string lastname;
+    bool hasTable;
+public:
+    TableTennisPlayer(const string &fn = "none",
+                      const string &ln = "none",
+                      bool ht = false);
+
+    void GetName() const;
+
+    bool HasTable() const { return this->hasTable; }
+
+    void ResetTable(bool v) { this->hasTable = v; }
+};
+
 #endif //CPPPRIMERPLUS_TABTENN0_H
