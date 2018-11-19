462a71500926b6f40100b9cef398a35f1b6dba4e
fengjiran <fengjiran@foxmail.com>
2018-11-19 20:26:01 +0800

    add chapter13 file

--- src/tabtenn0.cpp
+++ src/tabtenn0.cpp
@@ -19,15 +19,3 @@ void TableTennisPlayer::GetName() const {
 
 }
 
-RatedPlayer::RatedPlayer(unsigned int r,
-                         const string &fn,
-                         const string &ln,
-                         bool ht) : TableTennisPlayer(fn, ln, ht) {
-    rating = r;
-}
-
-RatedPlayer::RatedPlayer(unsigned int r,
-                         const TableTennisPlayer &tp) : TableTennisPlayer(tp) {
-    rating = r;
-
-}
--- src/tabtenn1.cpp
+++ src/tabtenn1.cpp
@@ -1,4 +1,31 @@
 //
 // Created by richard on 2018/11/19.
 //
+#include <iostream>
+#include "../header/tabtenn1.h"
+using std::cout;
+
+TableTennisPlayer::TableTennisPlayer(const string &fn,
+                                     const string &ln,
+                                     bool ht) : firstname(fn),
+                                                lastname(ln),
+                                                hasTable(ht) {}
+
+void TableTennisPlayer::GetName() const {
+    cout << lastname << ", " << firstname;
+
+}
+
+RatedPlayer::RatedPlayer(unsigned int r,
+                         const string &fn,
+                         const string &ln,
+                         bool ht) : TableTennisPlayer(fn, ln, ht) {
+    rating = r;
+}
+
+RatedPlayer::RatedPlayer(unsigned int r,
+                         const TableTennisPlayer &tp) : TableTennisPlayer(tp) {
+    rating = r;
+
+}
 
