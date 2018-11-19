9db3fbb9b403f86a4fe8b911d54ddcb4e0a9bea1
fengjiran <fengjiran@foxmail.com>
2018-11-19 20:34:57 +0800

    test new class and subclass

--- src/chapter13.cpp
+++ src/chapter13.cpp
@@ -2,3 +2,36 @@
 // Created by richard on 2018/11/19.
 //
 
+#include <iostream>
+#include "../header/tabtenn1.h"
+
+using std::cout;
+using std::endl;
+
+void usett1() {
+    TableTennisPlayer player1("Tara", "Boomdea", false);
+    RatedPlayer rplayer1(1140, "Mallory", "Duck", true);
+    rplayer1.GetName();
+
+    if (rplayer1.HasTable())
+        cout << ": has a table.\n";
+    else
+        cout << ": hasn't a table.\n";
+
+    player1.GetName();
+
+    if (player1.HasTable())
+        cout << ": has a table.\n";
+    else
+        cout << ": hasn't a table.\n";
+
+    cout << "Name: ";
+    rplayer1.GetName();
+    cout << "; Rating: " << rplayer1.Rating() << endl;
+
+    RatedPlayer rplayer2(1212, player1);
+    cout << "Name: ";
+    rplayer2.GetName();
+    cout << "; Rating: " << rplayer2.Rating() << endl;
+
+}
