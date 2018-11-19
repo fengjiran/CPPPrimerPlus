b8b19765bc433b5e5d9ca55aa6ba8683746575db
fengjiran <fengjiran@foxmail.com>
2018-11-19 20:15:38 +0800

    add another head file

---
+++ header/tabtenn1.h
@@ -0,0 +1,8 @@
+//
+// Created by richard on 2018/11/19.
+//
+
+#ifndef CPPPRIMERPLUS_TABTENN1_H
+#define CPPPRIMERPLUS_TABTENN1_H
+
+#endif //CPPPRIMERPLUS_TABTENN1_H
--- src/tabtenn0.cpp
+++ src/tabtenn0.cpp
@@ -25,3 +25,9 @@ RatedPlayer::RatedPlayer(unsigned int r,
                          bool ht) : TableTennisPlayer(fn, ln, ht) {
     rating = r;
 }
+
+RatedPlayer::RatedPlayer(unsigned int r,
+                         const TableTennisPlayer &tp) : TableTennisPlayer(tp) {
+    rating = r;
+
+}
