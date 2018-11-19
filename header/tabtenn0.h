d380fa7d53a6a7e304bb5f8a41ed275a4acc3101
fengjiran <fengjiran@foxmail.com>
2018-11-19 20:18:40 +0800

    modify

--- header/tabtenn0.h
+++ header/tabtenn0.h
@@ -26,22 +26,4 @@ public:
     void ResetTable(bool v) { this->hasTable = v; }
 };
 
-class RatedPlayer : public TableTennisPlayer {
-private:
-    unsigned int rating;
-public:
-    explicit RatedPlayer(unsigned int r = 0,
-                         const string &fn = "none",
-                         const string &ln = "none",
-                         bool ht = false);
-
-    RatedPlayer(unsigned int r, const TableTennisPlayer &tp);
-
-    unsigned int Rating() const { return rating; }
-
-    void ResetRating(unsigned int r) { rating = r; }
-
-
-};
-
 #endif //CPPPRIMERPLUS_TABTENN0_H
--- header/tabtenn1.h
+++ header/tabtenn1.h
@@ -5,4 +5,41 @@
 #ifndef CPPPRIMERPLUS_TABTENN1_H
 #define CPPPRIMERPLUS_TABTENN1_H
 
+#include <string>
+
+using std::string;
+
+class TableTennisPlayer {
+private:
+    string firstname;
+    string lastname;
+    bool hasTable;
+public:
+    explicit TableTennisPlayer(const string &fn = "none",
+                               const string &ln = "none",
+                               bool ht = false);
+
+    void GetName() const;
+
+    bool HasTable() const { return this->hasTable; }
+
+    void ResetTable(bool v) { this->hasTable = v; }
+};
+
+class RatedPlayer : public TableTennisPlayer {
+private:
+    unsigned int rating;
+public:
+    explicit RatedPlayer(unsigned int r = 0,
+                         const string &fn = "none",
+                         const string &ln = "none",
+                         bool ht = false);
+
+    RatedPlayer(unsigned int r, const TableTennisPlayer &tp);
+
+    unsigned int Rating() const { return rating; }
+
+    void ResetRating(unsigned int r) { rating = r; }
+};
+
 #endif //CPPPRIMERPLUS_TABTENN1_H
