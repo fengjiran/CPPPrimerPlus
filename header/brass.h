6ac75055bcfcf83c8f90b39c8462f850374d27cd
fengjiran <fengjiran@foxmail.com>
2018-11-24 20:50:00 +0800

    add BrassPlus class

--- header/brass.h
+++ header/brass.h
@@ -28,4 +28,27 @@ public:
     virtual ~Brass();
 };
 
+class BrassPlus : public Brass {
+private:
+    double max_loan;
+    double rate;
+    double owes_bank;
+public:
+    BrassPlus(const string &s = "Nullbody", long an = -1,
+              double bal = 0.0, double ml = 500,
+              double r = 0.11125);
+
+    BrassPlus(const Brass &ba, double ml = 500, double r = 0.11125);
+
+    virtual void ViewAcct() const;
+
+    virtual void WithDraw(double amt);
+
+    inline void ResetMax(double m) { max_loan = m; }
+
+    inline void ResetRate(double r) { rate = r; }
+
+    inline void ResetOwes() { owes_bank = 0; }
+};
+
 #endif //CPPPRIMERPLUS_BRASS_H
