13736c660a8b450aeb2aef094cc551eba71a67eb
fengjiran <fengjiran@foxmail.com>
2018-11-24 20:42:52 +0800

    add Brass class

--- header/brass.h
+++ header/brass.h
@@ -5,4 +5,27 @@
 #ifndef CPPPRIMERPLUS_BRASS_H
 #define CPPPRIMERPLUS_BRASS_H
 
+#include <string>
+
+using std::string;
+
+class Brass {
+private:
+    string full_name;
+    long acct_num;
+    double balance;
+public:
+    Brass(const string &s = "Nullbody", long an = -1, double bal = 0.0);
+
+    void Deposit(double amt);
+
+    virtual void WithDraw(double amt);
+
+    double Balance() const;
+
+    virtual void ViewAcct() const;
+
+    virtual ~Brass();
+};
+
 #endif //CPPPRIMERPLUS_BRASS_H
