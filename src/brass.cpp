44f6018e9893cbc6b3388413dfef0b5d26ddf46a
fengjiran <fengjiran@foxmail.com>
2018-11-24 21:49:02 +0800

    add two functions for show

--- src/brass.cpp
+++ src/brass.cpp
@@ -107,3 +107,12 @@ void BrassPlus::WithDraw(double amt) {
     restore(initialState, prec);
 
 }
+
+format setFormat() {
+    return cout.setf(std::ios_base::fixed, std::ios_base::floatfield);
+}
+
+void restore(format f, precis p) {
+    cout.setf(f, std::ios_base::floatfield);
+    cout.precision(p);
+}
