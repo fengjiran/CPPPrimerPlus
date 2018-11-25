8e605da28250f60872d84fe71e97b59075ec1d4c
fengjiran <fengjiran@foxmail.com>
2018-11-25 23:19:48 +0800

    test armadillo

---
+++ src/testArmadillo.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Richard on 2018/11/25.
+//
+#include <iostream>
+#include <armadillo>
+
+using namespace std;
+using namespace arma;
+
+void testArmadillo() {
+    mat A = randu<mat>(4, 5);
+    mat B = randu<mat>(4, 5);
+
+    mat C = A * trans(B);
+    cout << C << endl;
+}
