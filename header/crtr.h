109ea31c878295592efb6e8a6f27106c4dddccfc
fengjiran <fengjiran@foxmail.com>
2018-11-25 17:38:40 +0800

    add test crtr pattern cpp

--- header/crtr.h
+++ header/crtr.h
@@ -5,4 +5,23 @@
 #ifndef CPPPRIMERPLUS_CRTR_H
 #define CPPPRIMERPLUS_CRTR_H
 
+#include <iostream>
+
+using std::cout;
+
+template<typename T>
+class Base {
+public:
+    void show() const {
+        static_cast<const T *>(this)->show();
+    }
+};
+
+class Derived : public Base<Derived> {
+public:
+    void show() const {
+        cout << "Shown in Derived class.\n";
+    }
+};
+
 #endif //CPPPRIMERPLUS_CRTR_H
