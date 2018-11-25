b6ba2adf59d114895713253b12ee0f5952ddc836
fengjiran <fengjiran@foxmail.com>
2018-11-25 11:47:12 +0800

    add members

--- header/worker0.h
+++ header/worker0.h
@@ -5,4 +5,24 @@
 #ifndef CPPPRIMERPLUS_WORKER0_H
 #define CPPPRIMERPLUS_WORKER0_H
 
+#include <string>
+
+using std::string;
+
+class Worker {  // an abstract base class
+private:
+    string fullname;
+    long id;
+public:
+    Worker() : fullname("no one"), id(0L) {}
+
+    Worker(const string &s, long n) : fullname(s), id(n) {}
+
+    virtual ~Worker() = 0;
+
+    virtual void Set();
+
+    virtual void Show() const;
+};
+
 #endif //CPPPRIMERPLUS_WORKER0_H
