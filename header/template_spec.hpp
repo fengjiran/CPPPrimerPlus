9063ffda40c60835ca095f2b52f065f1660afa95
richard <fengjiran@foxmail.com>
2018-12-01 00:30:34 +0800

    test template spec

---
+++ header/template_spec.hpp
@@ -0,0 +1,13 @@
+//
+// Created by richard on 2018/12/1.
+//
+
+#ifndef CPPPRIMERPLUS_TEMPLATE_SPEC_HPP
+#define CPPPRIMERPLUS_TEMPLATE_SPEC_HPP
+
+template<typename T>
+int compare(const T &a, const T &b) {
+    //
+}
+
+#endif //CPPPRIMERPLUS_TEMPLATE_SPEC_HPP
